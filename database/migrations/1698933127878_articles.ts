5effd50b16b374512367bff493690161d45082e2
aryan-pirhashemi <aryanpirhashemi1346@gmail.com>
2023-11-02 18:34:40 +0330

    Updated the database schema and ran the migration.

--- database/migrations/1698933127878_articles.ts
+++ database/migrations/1698933127878_articles.ts
@@ -3,10 +3,13 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class extends BaseSchema {
   protected tableName = 'articles'
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-
+      table.string('title')
+      table.string('distinct').unique()
+      table.string('image')
+      table.string('content')
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
@@ -15,7 +18,7 @@ export default class extends BaseSchema {
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.dropTable(this.tableName)
   }
 }
