8aefcaef880b89fcfd1b0f214da2818af2904427
aryan-pirhashemi <aryanpirhashemi1346@gmail.com>
2023-11-02 12:58:48 +0330

    Learning how to name routes.

--- start/routes.ts
+++ start/routes.ts
@@ -8,18 +8,26 @@ Route.get('/', async ({ view }) => {
 //   return view.render('news/view')
 // })
 
-Route.on('/news').render('news/view')
+Route.on('/news').render('news/view').as('news.view')
+
+Route.post('/news', ({ response }) => {
+  return response.redirect('/news')
+}).as('news.create')
 
 Route.patch('/news/:id?', ({ params }) => {
   return { params }
-}).where('id', {
-  match: /^[0-9]+$/,
-  cast: (id) => Number(id),
 })
+  .where('id', {
+    match: /^[0-9]+$/,
+    cast: (id) => Number(id),
+  })
+  .as('news.update')
 
 Route.delete('/news/:id?', ({ params }) => {
   return { params }
-}).where('id', {
-  match: /^[0-9]+$/,
-  cast: (id) => Number(id),
 })
+  .where('id', {
+    match: /^[0-9]+$/,
+    cast: (id) => Number(id),
+  })
+  .as('news.delete')
