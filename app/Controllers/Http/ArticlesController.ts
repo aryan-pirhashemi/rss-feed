c4b011257ea43c87124cad23732d0b040c27dc01
aryan-pirhashemi <aryanpirhashemi1346@gmail.com>
2023-11-04 10:17:36 +0330

    Learnt storing in database and CSRF security

--- app/Controllers/Http/ArticlesController.ts
+++ app/Controllers/Http/ArticlesController.ts
@@ -5,6 +5,14 @@ export default class ArticlesController {
   public async view({ view }) {
     const articles = await Database.from('articles').select('*')
     // return articles
-    return view.render('news.view', { articles })
+    return view.render('news/view', { articles })
+  }
+  public create({ view }) {
+    return view.render('news/create')
+  }
+  public async store({ response, request }) {
+    const { title, content, image, distinct } = request.body()
+    await Database.table('articles').insert({ title, distinct, image, content })
+    return response.redirect().back()
   }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -1,20 +1,15 @@
 import Route from '@ioc:Adonis/Core/Route'
-import ArticlesController from 'App/Controllers/Http/ArticlesController'
 
 Route.get('/', async ({ view }) => {
   return view.render('welcome')
 })
 
-Route.get('/news', async (ctx) => {
-  // return articles
-  return new ArticlesController().view(ctx)
-}).as('news.view')
+Route.get('/news', 'ArticlesController.view').as('news.view')
+Route.get('/news/create', 'ArticlesController.create').as('news.create')
 
 // Route.on('/news').render('news/view').as('news.view')
 
-Route.post('/news', ({ response }) => {
-  return response.redirect('/news')
-}).as('news.create')
+Route.post('/news/create', 'ArticlesController.store').as('news.store')
 
 Route.patch('/news/:id?', ({ params }) => {
   return { params }
